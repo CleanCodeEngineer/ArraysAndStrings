0137134b86cc52c2c8c98ecace5001f7311a11cd
Sahar Dastitash <sahar.dastitash@gmail.com>
2023-11-19 16:16:50 -0800

    Updated Array notes.

--- ArraysAndStrings/Array.cs
+++ ArraysAndStrings/Array.cs
@@ -1,23 +1,19 @@
-﻿using System;
-
-namespace DataStructures
+﻿namespace DataStructures
 {
-    // Array is one of the simplest data structure that can save list of int, strings and any type of objects sequentialy in memory.
-    // integers in java takes 4 bytes of memory. we can access an item in the memory by it's index which is its address in the memory.
-    // Pros: Lookup O(1)
-    // Cons: Arrays have fixed size, and we should know from the beginning the size of the array that we'd like to assign to in the memory.
-    // we need to know ahead of time that how many items we want to store in an array, but if we don't know we have to make a guess. If our
-    // guess is too large we waste memory. if our guess is too small our array gets filled quickly then to add another item we have to resize the array.
-    // which means we have to allocate a larger array and copy all the items in th old array to the new one. Insert O(n)
-    // removing an item from the last index which is the Best case: Delete O(1) and in worst case scenario if we are removing an item from the first index in the array,
-    // move all the items to the left so we have n operations: Delete O(n)
-    // If we're not sure about the size of an array and we have insertion and deletion alot, we use linked list instead.
+    // An array is one of the simplest data structures that can store a list of integers, strings, and any type of objects sequentially in memory.
+    // In C#, integers typically take 4 bytes of memory. We can access an item in the memory by its index, which is its address in the memory.
+    // Pros: Constant-time lookup O(1).
+    // Cons: Arrays have a fixed size, and we need to know the size of the array from the beginning before assigning it in memory.
+    // We must determine in advance how many items we want to store in an array. If uncertain, we may need to make a guess. If our
+    // guess is too large, we waste memory; if it's too small, our array fills up quickly, and to add another item, we have to resize the array.
+    // Resizing involves allocating a larger array and copying all the items from the old array to the new one. Insertion time complexity becomes O(n).
+    // If uncertain about the size of an array and dealing with frequent insertions and deletions alot, a linked list may be a more suitable alternative.
     // Dynamic Array
     public class Array
     {
         private int[] items;
-        // the count of the items exist in the array
-        // every time we insert an item, we will increment the count
+        // Represents the count of items currently in the array.
+        // Every time we insert an item, we increment the count.
         private int count;
 
         public Array(int length)
@@ -25,13 +21,13 @@ namespace DataStructures
             items = new int[length];
         }
 
-        // now we have a dynamic array, as the input grows, it will resize
+        // Now we have a dynamic array; as the input grows, it will automatically resize.
         public void insert(int item)
         {
-            // If the array is full, resize it
-            if(count == items.Length)
+            // Resize the array if it is full  
+            if (count == items.Length)
             {
-                // Create a new array (twice the size)
+                // Create a new array with double the size to accomodate resizing.
                 var newItems = new int[count * 2];
 
                 // Copy all the existing items
@@ -53,37 +49,38 @@ namespace DataStructures
                 Console.WriteLine(items[i]);
         }
 
+        // Removing an item from the last index is the Best case: Deletion in O(1).
+        // In the worst case, if we are removing an item from the first index in the array,
+        // we need to move all the remaining items to the left, resulting in O(n) operations for Deletion.
         public void removeAt(int index)
         {
             // Validate the index
             if (index < 0 || index >= count)
                 throw new Exception();
-                // 2 3 4  i = 1 , 2
-                // 2 4
-                // Shift the items to the left to fill the hole
-                // [10, 30, 40]
-                // index: 1
-                // 1 <- 2
-                // 2 <- 3
-                for (int i = index; i < count; i++)
-                {
-                    items[i] = items[i + 1];
-                }
+            // Shift the remaining items to the left to fill the gap created by the removed element.
+            // [10, 30, 40]
+            // index: 1
+            // 1 <- 2
+            // 2 <- 3
+            for (int i = index; i < count; i++)
+            {
+                items[i] = items[i + 1];
+            }
 
-                // count is the total number of items in the array, not the size of the array.
-                count--;
+            // 'count' represents the total number of items in the array, not the size of the array.
+            count--;
         }
 
-        // Search operation
-        // run time complexity of this method is O(n)
-        // Best case the item is at the index 0 so O(1)
-        // Worst case scenario it's that the item is at the last index O(n)
-        // in the run time complexity we always want the worst case scenario so it's O(n)
+        // Search operation:
+        // The runtime complexity of this method is O(n).
+        // Best case: The item is at index 0, resulting in O(1).
+        // Worst case: The item is at the last index, resulting in O(n).
+        // In the context of runtime complexity analysis, we consider the worst-case scenario, making it O(n).
         public int indexOf(int item)
         {
             // If we find it, return index
             // Otherwise, return -1
-            for(int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
                 if (items[i] == item)
                     return i;
 
