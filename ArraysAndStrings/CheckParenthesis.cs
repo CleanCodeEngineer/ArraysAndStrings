5c3a74aaf4c0514524553858d3053e05e5114fd6
Sahar Dastitash <sahar.dastitash@gmail.com>
2025-05-31 11:25:05 -0700

    Improved check parenthesis

--- ArraysAndStrings/CheckParenthesis.cs
+++ ArraysAndStrings/CheckParenthesis.cs
@@ -3,27 +3,28 @@ namespace ArraysAndStrings
 {
     public class CheckParenthesis
     {
+        // Question: Given a string and the position of an opening parenthesis '(', write a function to find the position of the corresponding closing parenthesis ')'.
         // sentence: "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
         // Complexity: O(n) time, where n is the number of chars in the string. O(1) space.
         public static int FindClosingPosition(string sentence, int openPosition)
         {
-            int closingPos = 0;
             int openCounts = 1;
-            int index = openPosition;
+            int index = openPosition + 1;
 
-            while (openCounts != 0 && index < sentence.Length)
+            while (index < sentence.Length)
             {
-                index++;
-
                 if (sentence[index] == '(')
                     openCounts++;
                 else if (sentence[index] == ')')
                     openCounts--;
+
+                if (openCounts == 0)
+                    return index;
+
+                index++;
             }
 
-            closingPos = index;
-
-            return closingPos;
+            return -1; // Not found
         }
     }
 }
